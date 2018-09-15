3b004d58cddf08d7ca0ab220bdeaf00ea8e9d834
Gabriela Benejam <meyerserlen@gmail.com>
2018-09-15 16:56:05 +0200

    Adding experience section

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -7,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
-  constructor() { }
+  
+  public experienceArr:ExperienceData[] = [
+        {
+            title: "AEM Consultant",
+            company: "Blue Infinity",
+            city: "Geneva",
+            duties: "viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at varius vel pharetra vel turpis nunc eget lorem dolor sed viverra ",
+            startDate: "March 2013",
+            endDate: "Present"
+        },
+        {
+            title: "Junior Web Developer",
+            company: "Netcentric",
+            city: "Barcelona",
+            duties: "viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius duis at consectetur lorem donec massa sapien faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus arcu bibendum at varius vel pharetra vel turpis nunc",
+            startDate: "December 2011",
+            endDate: "March 2013"
+        }
+    ];
 
   ngOnInit() {
   }
